8f4fe4e89abd19fc72f7f9bbeae94aa0eeddd86d
https://bnelo12@github.com <s1623919@sms.ed.ac.uk>
2018-11-10 22:02:06 +0000

    Added logging in

--- webapp/app/routes.py
+++ webapp/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request
 
 from app import app
 # from app import mongo
@@ -6,24 +6,29 @@ from app import socket
 
 @app.route('/')
 def indexPage():
-    return render_template('index.html')
+    user = request.cookies.get('username')
+    return render_template('index.html', user=user)
 
 @app.route('/rent')
 def rentPage():
-    return render_template('rent.html')
+    user = request.cookies.get('username')
+    return render_template('rent.html', user=user)
 
 @app.route('/host')
 def hostPage():
-    return render_template('host.html')
+    user = request.cookies.get('username')
+    return render_template('host.html', user=user)
 
 @app.route('/login')
 def loginPage():
-    return render_template('login.html')
+    user = request.cookies.get('username')
+    return render_template('login.html', user=user)
 
 @app.route('/signup')
 def signupPage():
-    return render_template('signup.html')
+    user = request.cookies.get('username')
+    return render_template('signup.html', user=user)
 
 @app.route('/editor')
 def editorPage():
-    return render_template('editor.html')
+    return render_template('editor.html', user=user)
